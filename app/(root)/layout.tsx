6e06cb9aeb927679669411ba5164cc06eb8fbf73
Antarin Ghosal <antaringhosal@gmail.com>
2025-06-15 19:45:16 +0530

    Added dummy interview data and integrate dayjs dependency

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,18 @@
-import React, { ReactNode } from 'react'
+import { ReactNode } from 'react'
+
+import Image from 'next/image'
+import Link from 'next/link'
 
 const Rootlayout = ({ children } : {children: ReactNode}) => {
   return (
-    <div>
+    <div className="root-layout">
+      <nav>
+        <Link href="/" className="flex items-center gap-2">
+          <Image src="/logo.svg" alt="logo" height={32} width={38} />
+          <h2 className="text-primary-100">InterVueAI</h2>
+        </Link>
+      </nav>
+
       {children}
     </div>
   )
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,54 @@
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+import { dummyInterviews } from "@/constants";
+import { Inter } from "next/font/google";
 
 const page = () => {
   return (
-    <div>
-      Home Page
-    </div>
-  )
-}
+    <>
+      <section className="card-cta">
+        <div className="flex flex-col gap-6 max-w-lg">
+          <h2>Get Interview-Ready with AI-Powered practice & Feedback </h2>
 
-export default page
+          <p className="text-lg">
+            Practice on real interview questions & get instant feedback
+          </p>
+
+          <Button asChild className="btn-primary max-sm:w-full">
+            <Link href="/interview">Start an Interview</Link>
+          </Button>
+        </div>
+
+        <Image
+          src="/robot.png"
+          alt="robot"
+          width={400}
+          height={400}
+          className="max-sm:hidden"
+        />
+      </section>
+
+      <section className="flex flex-col gap-6 mt-8">
+        <h2>Your Interviews</h2>
+
+        <div className="interviews-section">
+          {dummyInterviews.map((interview) => (
+            <InterviewCard />
+          ))}
+        </div>
+      </section>
+
+      <section className="flex flex-col gap-6 mt-8">
+        <h2>Take an interview</h2>
+
+        <div className="interviews-section">
+          <p>There are no interviews available</p>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default page;
