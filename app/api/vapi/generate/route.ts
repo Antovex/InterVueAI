d71810d1c6af1e5f563717a63aefe7811c5cae86
Antarin Ghosal <antaringhosal@gmail.com>
2025-06-23 20:44:34 +0530

    Refactor API

--- app/api/vapi/generate/route.ts
+++ app/api/vapi/generate/route.ts
@@ -17,11 +17,8 @@ export async function GET() {
 }
 
 export async function POST(request: Request) {
-    // const { type, role, level, techstack, amount, userid } =
-    //     await request.json();
-    const body = await request.json();
-    console.log("Received body from VAPI:", body);
-    const { type, role, level, techstack, amount, userid } = body;
+    const { type, role, level, techstack, amount, userid } =
+        await request.json();
 
     try {
         const { text: questions } = await generateText({
